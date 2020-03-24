0b38203a05433b7862ea4ccb59b4a7447d97ef83
Abhilash Paul <paul.abhilash@hotmail.com>
2020-03-24 10:16:20 -0600

    implements alberta tax calculation

--- src/main/java/com/example/taxcalculator/Controller/TaxCalculatorController.java
+++ src/main/java/com/example/taxcalculator/Controller/TaxCalculatorController.java
@@ -6,6 +6,7 @@ package com.example.taxcalculator.Controller;
   Date: 23 Mar 2020
 */
 
+import com.example.taxcalculator.model.AlbertaTax;
 import com.example.taxcalculator.model.FederalTax;
 import com.example.taxcalculator.model.TaxQuery;
 import com.example.taxcalculator.model.TaxResult;
@@ -25,6 +26,19 @@ public class TaxCalculatorController {
 
             //calculate taxes
             double federalTax = FederalTax.calculateFederalTax(taxQuery);
+            double taxableIncome = taxQuery.getEmploymentIncome()+taxQuery.getSelfEmploymentIncome()+taxQuery.getOtherIncome()+taxQuery.getCapitalGain()+taxQuery.getEligibleDividend()+taxQuery.getNonEligibleDividend();
+
+            double provincialTax=0;
+            String province = taxQuery.getProvince();
+            switch (province){
+                case "AB": provincialTax = AlbertaTax.calculateAlbertaTax(taxQuery);
+            }
+
+            taxResult.setTaxableIncome(taxableIncome);
+            taxResult.setProvincialTax(provincialTax);
+            taxResult.setTotalTax(federalTax+provincialTax);
+            taxResult.setAverageTaxRate((federalTax+provincialTax)/taxableIncome);
+
 
             //update result object
             taxResult.setFederalTax(federalTax);
